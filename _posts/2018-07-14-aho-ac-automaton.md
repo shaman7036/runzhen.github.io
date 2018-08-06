b2df46e9eee5dbc222769ce7debd18dcc13de033
Runzhen Wang <runzhew@g.clemson.edu>
2018-08-06 00:27:00 -0700

    aho

--- _posts/2018-07-14-aho-ac-automaton.md
+++ _posts/2018-07-14-aho-ac-automaton.md
@@ -46,12 +46,68 @@ Trie 建立好之后大概是这个样子：
 
 ### 给每个节点添加失败指针
 
-这是非常核心的一步，大概的思路是这样的。
+这是非常核心的一步，整体的思路是这样的。
 
 > 构造失败指针的方法：假设当前节点上的字母为 ‘a’，沿着父节点的失败指针走，直到走到一个节点，他的子节点中也有字母为 'a' 的节点。然后把当前节点的失败指针指向那个子节点。如果一直走到了root都没找到，那就把失败指针指向root。
 
 
-那么再具体一点，
+那么再具体一点，有以下几个步骤。
+
+- 对于整个 Trie 树，使用`广度优先搜索 BFS` 来处理每个节点。
+- root 节点的子节点特殊处理， 先将 root 的子节点的失败路径直接指向 root 节点。
+
+
+算法结构：
+
+- 先把 root 节点加入队列。
+- 进入 while 循环，直到 队列为空
+- 取队列头元素，构造它的失败指针
+- 再将它的子节点加入队列
 
 
 ![aho1](/image/2018/aho2.jpg){:height="300" width="300"}
+
+一个添加了失败指针的 Trie 树如上图所示。
+
+### 在输入文本中搜索
+
+- 从root节点开始，根据输入文本的字符沿着自动机向下移动。
+- 读入的字符在当前节点没有指向的子节点时，走失败路径。如果走失败路径走到了root节点，则跳过该字符，处理下一个字符。
+- 如果当前节点有终结符，则表明命中了一个模式串。（这一步可以优化为是否最长匹配）
+
+### 实现
+
+最后给出一个我自己的 Go 语言实现，最近学习了一下 Golang, 顺便拿来练练手。
+
+[aho.go](/image/2018/aho.go)
+
+
+一个简单的 test 代码检验结果是否正确。
+
+```
+func TestBasic(t *testing.T) {
+    trie := New()
+    trie.Add("fat", 1)
+    trie.Add("fare", 1)
+    trie.Add("hat", 1)
+    trie.Add("are", 1)
+
+    trie.AhoBuilder()
+
+    trie.Find("fatehatfare")
+}
+```
+
+
+## AHO 的优化
+
+优化要么就是时间，要么就是空间。 Trie 的实现就有链表指针方式，三数组实现，双数组实现，HAT，burst trie等。
+
+具体可以参考下面列出的资料。
+
+- [前缀树匹配(Double Array Trie)](https://turbopeter.github.io/2013/09/02/prefix-match/)
+- [Trie树的数组实现原理](http://blog.jqian.net/post/trie.html)
+- [AC 自动机在 Tengine 中的应用](http://blog.aka-cool.net/blog/2013/08/03/aho-corasick-automaton-in-tengine/)
+
+
+
---
+++ image/2018/aho.go
@@ -0,0 +1,179 @@
+package trie
+
+import "fmt"
+
+type Node struct {
+	val    rune
+	termi  bool // a key's termination
+	depth  int
+	meta   interface{}
+	parent *Node
+	child  map[rune]*Node
+	fail   *Node
+	saved  []string
+}
+
+type Trie struct {
+	root *Node
+	size int
+}
+
+// Create a new trie with an initialized root node
+func New() *Trie {
+	return &Trie{
+		root: &Node{
+			child: make(map[rune]*Node),
+			depth: 0,
+			fail:  nil},
+		size: 0,
+	}
+}
+
+// return trie root
+func (t *Trie) Root() *Node {
+	return t.root
+}
+
+// Add the key to the trie.
+func (t *Trie) Add(key string, meta interface{}) *Node {
+	t.size++
+	runes := []rune(key)
+
+	node := t.root
+
+	for _, r := range runes {
+		if n, ok := node.child[r]; ok {
+			node = n
+		} else {
+			node = node.NewChild(r, nil, false)
+		}
+	}
+	node.termi = true
+	node.saved = append(node.saved, key)
+	//fmt.Printf("add %s \n", key)
+	node.meta = meta
+
+	return node
+}
+
+func (n *Node) NewChild(val rune, meta interface{}, termi bool) *Node {
+	node := &Node{
+		val:    val,
+		termi:  termi,
+		meta:   meta,
+		parent: n,
+		child:  make(map[rune]*Node),
+		depth:  n.depth + 1,
+		fail:   nil,
+		saved:  make([]string, 0),
+	}
+	n.child[val] = node
+
+	return node
+}
+
+func (n *Node) Children() map[rune]*Node {
+	return n.child
+}
+
+// AhoBuilder
+func (t *Trie) AhoBuilder() {
+	queue := make([]*Node, 0)
+	queue = append(queue, t.Root())
+
+	var queueHead *Node
+	var p *Node
+
+	for {
+		if len(queue) == 0 {
+			break
+		}
+
+		// at the beginning, put the Root to queue
+		// root's fail = nil
+		queueHead = queue[0]
+
+		// iterate current Node's children
+		for r, n := range queueHead.Children() {
+
+			p = queueHead.fail
+
+			// root's children fail pointer points to root
+			if queueHead == t.Root() {
+				n.fail = t.Root()
+			} else {
+
+				for {
+					if p != nil && p.Children()[r] == nil {
+						p = p.fail
+					} else {
+						break
+					}
+				}
+
+				if p != nil {
+					n.fail = p.Children()[r]
+				} else {
+					n.fail = t.Root()
+				}
+			}
+
+			queue = append(queue, n)
+		}
+		queue = queue[1:]
+	}
+
+}
+
+func printSaved(n *Node, i int, ch string) {
+	fmt.Printf("Node %p hit at str[%d] \"%s\": ", n, i, ch)
+	for _, k := range n.saved {
+		fmt.Printf("%s, ", k)
+	}
+	fmt.Printf("\n")
+}
+
+// Finds the key
+func (t *Trie) Find(key string) bool {
+
+	str := []rune(key)
+
+	p := t.Root()
+	var ch rune
+
+	for i := 0; i < len(str); i++ {
+		ch = str[i]
+
+		for {
+			if p.Children()[ch] == nil && p != t.Root() {
+				p = p.fail
+			} else {
+				break
+			}
+		}
+
+		p = p.Children()[ch]
+
+		if p == nil {
+			p = t.Root()
+		} else {
+			if p.termi == true {
+				printSaved(p, i, string(ch))
+				// 匹配该节点后,沿其失败指针回溯,判断其他节点是否匹配
+				temp := p
+				for {
+					temp = temp.fail
+
+					if temp == nil {
+						break
+					}
+
+					if temp.termi == true {
+						printSaved(temp, i, string(""))
+					}
+				}
+			}
+		}
+	}
+	return true
+}
